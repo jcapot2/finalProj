b1ce8209df131cccb6d876189d88f016df06b648
Jovek <joetomorrow895@gmail.com>
2019-12-03 16:15:45 -0600

    added menu

--- app/src/main/java/om/example/finalproj/MainActivity.java
+++ app/src/main/java/om/example/finalproj/MainActivity.java
@@ -4,15 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 //nothing much, hbu
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
     GraphView graph;
@@ -24,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button start = findViewById(R.id.button);
+        Button start = findViewById(R.id.graphButton);
         start.setOnClickListener(unused -> startClicked());
     }
     private void startClicked() {
